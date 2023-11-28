1a965075d9d8b15527a1c6e906c79ee23ce08d25
Mark Cartwright <markcartwright65@gmail.com>
2023-11-28 15:58:53 +0000

    Refactored method out of Grid class
    setHover, delselectCurrentHoverCellKey, startSearch, stopSearch,
    isSearchInProgress, setStartCell, setGoalCell, , updateOpenList,
    updateClosedList methods have been moved to CanvasState Class.

--- Grid.js
+++ Grid.js
@@ -1,3 +1,6 @@
+// TODO: Implement either a state or strategy design pattern
+// to allow for an "edit" mode and a "search" mode with different behaviours
+// e.g. changing the UI, disabling buttons, turning off hover effect.
 class Grid {
     constructor(mapText) {
         // remove trailing whitespace from mapText
@@ -66,33 +69,6 @@ class Grid {
         }
     }
 
-    setHover(mouseX, mouseY , hoverVal) {
-        let gridReferenceCoords = Grid.getGlobalMouseToGridReferenceCoords(mouseX, mouseY);
-        let key = Grid.getGridCellKey(gridReferenceCoords.row, gridReferenceCoords.col); 
-        if (key in this.grid && key !== this.startCell && key !== this.goalCell)
-        {
-            // deselect current active hover cell
-            if (this.currentHoverCellKey !== null){
-                this.deselectCurrentHoverCellKey();
-            }
-
-            // assign new active hover cell
-            this.currentHoverCellKey = key;
-            this.grid[key] = {...this.grid[key],
-               "hover" : hoverVal
-            }
-            this.grid[key].value = WHITE_COLOUR;
-        }
-    }
-
-    deselectCurrentHoverCellKey() {
-        if (this.currentHoverCellKey !== null && this.currentHoverCellKey in this.grid){
-            this.grid[this.currentHoverCellKey].hover = false;
-            this.grid[this.currentHoverCellKey].value = this.grid[this.currentHoverCellKey].baseValue
-            this.currentHoverCellKey = null;
-        }
-    }
-
     setColor(key, colour) {
         if  (key in this.grid) {
             this.grid[key].value = colour;
@@ -103,70 +79,4 @@ class Grid {
         return row < 0 || col < 0 || (col + size) >= this.width || (row + size) >= this.height;
     }
 
-    startSearch(config) {
-        this.searchSolution = new Search_Solution(this, config);
-        let sRow = parseInt(this.startCell.split(",")[0]);
-        let sCol = parseInt(this.startCell.split(",")[1]);
-        let gRow = parseInt(this.goalCell.split(",")[0]);
-        let gCol = parseInt(this.goalCell.split(",")[1]);
-        this.searchSolution.startSearch(sRow, sCol, gRow, gCol);
-    }
-
-    stopSearch() {
-        this.searchSolution = null;
-    }
-
-    isSearchInProgress() {
-        return this.searchSolution !== null;
-    }
-
-    setStartCell(mouseX, mouseY) {
-        if (this.startCell === null) {
-            let gridReferenceCoords = Grid.getGlobalMouseToGridReferenceCoords(mouseX, mouseY);
-            this.startCell = Grid.getGridCellKey(gridReferenceCoords.row, gridReferenceCoords.col);
-            if (this.currentHoverCellKey === this.startCell){
-                this.deselectCurrentHoverCellKey();
-            }
-            this.setColor(Grid.getGridCellKey(gridReferenceCoords.row, gridReferenceCoords.col), WHITE_COLOUR);
-            console.log(`Start cell: ${this.startCell}`);
-        }
-    }
-
-    setGoalCell(mouseX, mouseY) {
-        if (this.goalCell === null) {
-            let gridReferenceCoords = Grid.getGlobalMouseToGridReferenceCoords(mouseX, mouseY);
-            this.goalCell = Grid.getGridCellKey(gridReferenceCoords.row, gridReferenceCoords.col);
-            if (this.currentHoverCellKey === this.goalCell){
-                this.deselectCurrentHoverCellKey();
-            }
-            this.setColor(Grid.getGridCellKey(gridReferenceCoords.row, gridReferenceCoords.col), PURPLE_COLOUR);
-            console.log(`Goal cell: ${this.goalCell}`);
-        }
-    }
-    
-    updatePath() {
-        if (this.isSearchInProgress()){
-            for (let key of this.searchSolution.path) {
-                this.setColor(key, WHITE_COLOUR);
-            }
-        }
-    }
-
-    updateOpenList() {
-        if (this.isSearchInProgress()){
-            for (let node of this.searchSolution.getOpen()){
-                let key = Grid.getGridCellKey(node.row, node.col);
-                this.setColor(key, OPEN_LIST_COLOUR);
-            }
-        }
-    }
-
-    updateClosedList() {
-        if (this.isSearchInProgress()){
-            for (let node of this.searchSolution.getClosed()){
-                let key = Grid.getGridCellKey(node.row, node.col);
-                this.setColor(key, CLOSED_LIST_COLOUR);
-            }
-        }
-    }
 }
--- Search_Solutions.js
+++ Search_Solutions.js
@@ -15,7 +15,6 @@ class Search_Solution {
 
 
         this.inProgress = false;
-        this.name = "Student";
 
         this.path = [];
         this.open = [];
